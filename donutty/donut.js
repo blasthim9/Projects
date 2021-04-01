32f4e7a5839100da093eed5e0e2bba33a2e5f4eb
dshi <dshi2107@gmail.com>
2021-04-01 03:34:28 -0400

    pls fix rotation halp

--- donutty/donut.js
+++ donutty/donut.js
@@ -1,11 +1,6 @@
 "use strict";
-var term = require( 'terminal-kit' ).terminal ;
+var term = require("terminal-kit").terminal;
 
-term.grabInput( { mouse: 'motion' } ) ;
-
-term.on( 'mouse' , function( name , data ) {
-    console.log( "'mouse' event:" , name , data ) ;
-} ) ;
 class TSScreen {
   #children = {};
   constructor(col, row, render = "3d", background = "#", colSpread = " ") {
@@ -28,14 +23,16 @@ class TSScreen {
   clear() {
     this.display = new Array(this.row * this.col).fill(this.background);
   }
-  update(){
-    for(let child in this.#children){
+  update() {
+    for (let child in this.#children) {
+     
       this.#children[child].update(this);
     }
   }
-  tsupdate(x,y,z,color='@') {
+  tsupdate(x, y, z, color = "@") {
     let scrnx = x + Math.ceil(this.col / 2 - this.camCenter.x);
-    let scrny = y + Math.ceil(this.row / 2 - this.camCenter.y);
+    let scrny = y + 1 + Math.ceil(this.row / 2 - this.camCenter.y);
+    
     if (!(scrnx >= this.col || scrny >= this.row || scrnx <= 0 || scrny <= 0)) {
       this.display[this.toScreenPos(scrnx, scrny)] = color;
     }
@@ -78,15 +75,16 @@ class Point {
     let [x, y, z] = [this.x, this.y, this.z];
     if (axis === "x") {
       y = this.y * costheta - this.z * sintheta + origin.y;
-      z = this.z * sintheta + this.z * costheta + origin.z;
+      z = this.y * sintheta + this.z * costheta + origin.z;
     } else if (axis === "y") {
-      x = this.x * costheta + this.z * sintheta + origin.x;
-      z = this.z * costheta + this.x * sintheta + origin.z;
+      x = this.z * sintheta + this.z * costheta + origin.x;
+      z = this.z * costheta - this.x * sintheta + origin.z;
     } else {
       x = this.x * costheta - this.y * sintheta + origin.x;
       y = this.x * sintheta + this.y * costheta + origin.y;
     }
     [this.x, this.y, this.z] = [x, y, z];
+   
   }
   toRad(angle) {
     return (angle * Math.PI) / 180;
@@ -158,22 +156,46 @@ class Circle extends Geometry {
     this.#radius = rad;
     this.init();
   }
+  rotateSegs(deg, face = "z", origin = Point) {
+    for (let i in this.#segments) {
+      this.#segments[i].drotate(deg, face, origin);
+    }
+  }
   update(canvasContext = TSScreen) {
     for (let i of c1.getSegments()) {
-      canvasContext.tsupdate(Math.floor(i.x), Math.floor(i.y),Math.floor(i.z),this.material.color);
+      canvasContext.tsupdate(
+        Math.floor(i.x),
+        Math.floor(i.y),
+        Math.floor(i.z),
+        this.material.color
+      );
     }
   }
 }
 
-let canvas = new TSScreen(20,20);
-let c1 = new Circle(0, 0, 0, 5, 1);
+let canvas = new TSScreen(50, 50);
+let c1 = new Circle(0, 0, 0, 15, 1);
 canvas.attachChild(c1);
-console.log(canvas.getChildren());
-
 
 
 canvas.update();
 
 canvas.tsdraw();
 
+let deg = 0;
 
+let interval = setInterval(()=>{
+  console.clear()
+ 
+  //c1.rotateSegs(deg, "x", c1.position);
+  c1.rotateSegs(deg, "x", c1.position);
+  c1.rotateSegs(deg, "z", c1.position);
+  canvas.clear();
+  canvas.update();
+  canvas.tsdraw();
+  deg+=.1
+  console.log(deg)
+  if(deg === 360) clearInterval(interval)
+},100)
+
+//console.log(c1.getSegments())
